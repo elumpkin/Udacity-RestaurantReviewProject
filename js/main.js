2373e0eddee70864c7f8c80f11533e8d028662b5
Ebonie Lumpkin <ebonie.b.lumpkin@gmail.com>
2018-12-02 01:19:59 -0500

    updating for review comments

--- js/main.js
+++ js/main.js
@@ -162,6 +162,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = "Image of the scence inside of the restaurant";
   li.append(image);
 
   const name = document.createElement('h1');
